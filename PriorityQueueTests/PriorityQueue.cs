c78871a04167577d01592e9c8a38487cebf2d718
Marianne Albuquerque da Silva <marikell_silva@hotmail.com>
2017-05-21 12:02:31 -0300

    Alterei apenas o Program.cs

---
+++ PriorityQueueTests/PriorityQueue.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PriorityQueueTests
+{
+    /*Lista de Prioridade que contém um dictionary<string,int>, no qual a chave é uma string(identificação do Node) e o valor é um int (prioridade do Node)*/
+    public class PriorityQueue
+    {
+        #region "Atributos"
+        private Dictionary<string, int> Dictionary;
+        #endregion
+
+        #region "Construtor"
+        public PriorityQueue()
+        {
+            Dictionary = new Dictionary<string, int>();
+        }
+        #endregion
+
+        #region "Métodos Públicos"
+        /*Método que ordena a lista de prioridade.*/
+        public void Order()
+        {
+            var items = from pair in Dictionary
+                        orderby pair.Value ascending
+                        select pair;
+            Dictionary = new Dictionary<string, int>();
+            foreach (KeyValuePair<string, int> pair in items)
+            {
+                Dictionary.Add(pair.Key, pair.Value);
+            }
+        }
+        /*Método que lista todos os elementos da lista de prioridade */
+        public void List()
+        {
+            foreach (var item in Dictionary)
+            {
+                Console.WriteLine("Value:" + item.Value);
+            }
+        }
+        /*Método que adiciona um elemento na lista de prioridade*/
+        public void Enqueue(string key, int value)
+        {
+            Dictionary.Add(key, value);
+            Order();
+
+        }
+        /*Método que remove um elemento da lista de prioridade, baseado no FIFO*/
+        public int Dequeue()
+        {
+            Dictionary.Remove(Dictionary.First().Key);
+            return Dictionary.First().Value;
+        }
+        #endregion
+    }
+}
---
+++ PriorityQueueTests/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PriorityQueueTests
+{
+    /*Classe de Testes relacionados a PriorityQueue*/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PriorityQueue list = new PriorityQueue();
+
+            list.Enqueue("123", 30);
+            list.Enqueue("456", 10);
+            list.Enqueue("789", 5);
+            list.Enqueue("1020", 82);
+            list.Enqueue("5615", 55);
+
+            list.List();
+            list.Dequeue();
+            list.Enqueue("44554", 1);
+            Console.WriteLine("\n");
+            list.List();
+
+            Console.ReadKey();
+
+        }
+    }
+}
